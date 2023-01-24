c9c14dd65cd1e42f4b64976a717fe8162e2acf4f
Kyryl R <kyryl.ryabov@gmail.com>
2023-01-24 13:57:04 +0200

    Added transfer ownership

--- deploy/1_core.migration.js
+++ deploy/1_core.migration.js
@@ -3,14 +3,16 @@
 const ERC1967Proxy = artifacts.require("ERC1967Proxy");
 const Swapica = artifacts.require("Swapica");
 
-// TODO: change signer addresses
-const signers = ["0xE461aa915538B81BA17995DF5FEDB96640f10BDE"];
+const signers = ["0xA2159b461D35E2c2faA5A561dE051ba8CCC1B9e7"];
+const newOwner = "0x53638975BC11de3029E46DF193d64879EAeA94eB";
 
 module.exports = async (deployer, logger) => {
   const swapica = await deployer.deploy(Swapica);
   const proxyS = await deployer.deploy(ERC1967Proxy, swapica.address, "0x");
 
   logger.logTransaction(await (await Swapica.at(proxyS.address)).__Swapica_init(signers), "Initialize Swapica");
+
+  logger.logTransaction(await (await Swapica.at(proxyS.address)).transferOwnership(newOwner), "Transfer Ownership");
 
   logger.logContracts(["Swapica implementation", swapica.address], ["Swapica proxy", proxyS.address]);
 };
